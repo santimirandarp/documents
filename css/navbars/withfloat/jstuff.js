9fdcaf08762f8d418426cf6c7ad1ce0cc050f43a
Santiago Miranda <santimirandarp@gmail.com>
2020-02-21 17:57:46 +0000

    a few new docs and updates

---
+++ css/navbars/withfloat/jstuff.js
@@ -0,0 +1,17 @@
+      function myToggle() {
+        var outcode = document.getElementById("out");
+        if (outcode.style.display == "none") {
+          outcode.style.display = "block";
+        } else {
+          outcode.style.display = "none";
+        }
+      }
+
+      function myToggle1() {
+        var outcode1 = document.getElementById("outNum");
+        if (outcode1.style.display == "none") {
+          outcode1.style.display = "block";
+        } else {
+          outcode1.style.display = "none";
+        }
+      }
