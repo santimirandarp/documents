099fdfdb5501d868fdb68024b3967928eb302981
Santiago Miranda <santimirandarp@gmail.com>
2020-03-18 15:02:23 +0000

    deleted superfluos files II

--- replace.py
+++
@@ -1,8 +0,0 @@
-import re, glob
-
-for i in glob.glob('./html_wlink/*.html'):
-  with open(i, 'r') as myfile :
-    filedata = myfile.read()
-  filedata = filedata.replace('../css/', '')
-  with open(i, 'w') as file:
-    file.write(filedata)
