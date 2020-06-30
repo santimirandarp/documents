53eb1fd8e97a74f3795ab3c2de9e99cff8d8c43e
santi <santimir.me@gmail.com>
2020-06-30 18:47:51 +0100

    Created a few text files as remainders for web stf.

--- example.sh
+++ example.sh
@@ -5,47 +5,35 @@ echo "$0-notliteral"
 #Single quotes won't treat variables as variables but as words
 #Double quotes will allow you to do substitution (that is include variables within the setting of the value).
 
-#How to declare a variable in Bash
-replacement="WORLD~"
-
-#Random example to learn
-#thesource="$HOME/Desktop/thefile"
-#thedestination="$HOME/Downloads/thefile"
-#touch $thesource 
-#cp $thesource $thedestination
-
-#sometimes we want to
-#POSIX shells such as Bash have the globbing feature specifically for this purpose -- to allow the shell to expand patterns into a list of matching filenames.
-#for file in *.txt; do   
-#    echo "$file" 
-#done
-##That's ok but problem is that, even if there are no .txt files, the loop runs once.
-
-
+#Variables in Bash
+	#never leave blank spaces around the = sign in bash
+replacement="WORLD~" # "" for string
+singer=(joan manuel serrat) # () for array
+bettersinger=("joan" "manuel" "serrat")
 #command substitution
-#saves an output of  command in a variable.
-#example myvar=$( ls /etc | wc -l )
+	#saves an output of  command in a variable.
+myvar=$( ls . | wc -l )
+echo "number of dirs is %d" "$myvar"
 #Arrays and iteration.
 
-singer=(joan manuel serrat)
+#iterate over elements
 for name in "${singer[@]}" 
 do
 	echo "$name"
 done
-#I mean: that rare syntax is expanded into something like this:
-random="hello I am santi"
-for word in $random
-do
-	echo "$word"
-done
 
-# Iterate over the indexes of "host".
+# Iterate over the indexes.
 #for idx in "${!singer[@]}" 
 #do
 #	printf "Singer name %d is %s\n" "$idx" "${singer[idx]}"
 #done
 
-#Loops with globbing in bash.
+#Loops using globbing
+#loops
+#for file in *.txt; do   
+#    echo "$file" 
+#done
+##That's ok but problem is that, even if there are no .txt files, the loop runs once.
 for file in *.txt; do
 	[ -e "$file" ] || continue #if the test is false, it goes on to continue (it doesnt otherwise) and jumps off the loop.
     echo "$file"
