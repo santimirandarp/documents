cb0563012fa844c15d529443266d353fb3a628b3
Santi <santimirandarp@gmail.com>
2020-09-20 05:15:00 +0100

    working doc page

--- pages/assets/js/index.js
+++ pages/assets/js/index.js
@@ -1,12 +1,26 @@
 const html = document.querySelector('html')
-const body = document.body
-const button = document.createElement('button')
-button.textContent='Color Mode'
-button.classList.add('topButton', 'darkMode')
-body.prepend(button)
+const setColorButton = document.createElement('button')
+const navigationLinks=document.querySelectorAll('#navigation > dt')
 
+//add a button as a first child
+setColorButton.textContent='Color Mode'
+setColorButton.classList.add('topButton', 'darkMode')
+document.body.prepend(setColorButton)
+//click on button adds a class to the html body
 const toggleColorMode = () =>{
   html.classList.toggle('darkMode')
-  button.classList.toggle('darkMode')
+  document.body.classList.toggle('darkMode')
+  setColorButton.classList.toggle('darkMode')
 }
-button.addEventListener('click', toggleColorMode)
+setColorButton.addEventListener('click', toggleColorMode)
+
+//click on index item, triggers an event (shows sublinks)
+function addShowLink(e){
+  console.log(e.target)
+  const element = e.target.nextElementSibling
+  element.style.height=="0px"||element.style.height==0?
+    element.style.height="auto":
+    element.style.height="0px"
+
+}
+navigationLinks.forEach(link=>link.addEventListener("click", addShowLink))
