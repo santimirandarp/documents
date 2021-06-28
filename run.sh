15276dc1e1c6a7199caf625b8ab3c2cfda7bf5dc
sm <santimirandarp@gmail.com>
2021-06-28 19:37:02 +0200

    bash script

---
+++ run.sh
@@ -0,0 +1,12 @@
+cd md        
+for directory in *; do
+  echo "the directory is $directory\n\n"
+  cd $directory/
+  for file in *.md; do
+    thename=$(basename $file .md)
+    echo $thename
+    pandoc $thename.md -o $thename.html
+    done
+   mv -iv *.html ../../html/$directory/
+   cd ..
+done
